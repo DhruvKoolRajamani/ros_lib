759f7ef58a7da01e2673530168c9b810b73a00c9
Dhruv Kool Rajamani <dhruvkoolrajamani@gmail.com>
2019-11-30 05:00:20 -0500

    Added motor message

---
+++ motor_msg/motor_desired.h
@@ -0,0 +1,56 @@
+#ifndef _ROS_motor_msg_motor_desired_h
+#define _ROS_motor_msg_motor_desired_h
+
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "ros/msg.h"
+#include "std_msgs/Header.h"
+#include "std_msgs/UInt8.h"
+#include "std_msgs/Float32.h"
+
+namespace motor_msg
+{
+
+  class motor_desired : public ros::Msg
+  {
+    public:
+      typedef std_msgs::Header _header_type;
+      _header_type header;
+      typedef std_msgs::UInt8 _motor_id_type;
+      _motor_id_type motor_id;
+      typedef std_msgs::Float32 _measured_force_type;
+      _measured_force_type measured_force;
+
+    motor_desired():
+      header(),
+      motor_id(),
+      measured_force()
+    {
+    }
+
+    virtual int serialize(unsigned char *outbuffer) const
+    {
+      int offset = 0;
+      offset += this->header.serialize(outbuffer + offset);
+      offset += this->motor_id.serialize(outbuffer + offset);
+      offset += this->measured_force.serialize(outbuffer + offset);
+      return offset;
+    }
+
+    virtual int deserialize(unsigned char *inbuffer)
+    {
+      int offset = 0;
+      offset += this->header.deserialize(inbuffer + offset);
+      offset += this->motor_id.deserialize(inbuffer + offset);
+      offset += this->measured_force.deserialize(inbuffer + offset);
+     return offset;
+    }
+
+    const char * getType(){ return "motor_msg/motor_desired"; };
+    const char * getMD5(){ return "01823730f3e4576034981b3cc9bfd1e3"; };
+
+  };
+
+}
+#endif
