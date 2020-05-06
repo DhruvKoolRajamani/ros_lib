4a069049ff0a9c48b540f738fcafd881fa35c8bb
Dhruv Kool Rajamani <dhruvkoolrajamani@gmail.com>
2020-05-05 22:33:53 -0400

    changed imu_light back to float32

--- imu_msg/imu_light.h
+++ imu_msg/imu_light.h
@@ -12,8 +12,8 @@ namespace imu_msg
   class imu_light : public ros::Msg
   {
     public:
-      int16_t accelerometer[3];
-      int16_t gyroscope[3];
+      float accelerometer[3];
+      float gyroscope[3];
 
     imu_light():
       accelerometer(),
@@ -26,22 +26,26 @@ namespace imu_msg
       int offset = 0;
       for( uint32_t i = 0; i < 3; i++){
       union {
-        int16_t real;
-        uint16_t base;
+        float real;
+        uint32_t base;
       } u_accelerometeri;
       u_accelerometeri.real = this->accelerometer[i];
       *(outbuffer + offset + 0) = (u_accelerometeri.base >> (8 * 0)) & 0xFF;
       *(outbuffer + offset + 1) = (u_accelerometeri.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_accelerometeri.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_accelerometeri.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->accelerometer[i]);
       }
       for( uint32_t i = 0; i < 3; i++){
       union {
-        int16_t real;
-        uint16_t base;
+        float real;
+        uint32_t base;
       } u_gyroscopei;
       u_gyroscopei.real = this->gyroscope[i];
       *(outbuffer + offset + 0) = (u_gyroscopei.base >> (8 * 0)) & 0xFF;
       *(outbuffer + offset + 1) = (u_gyroscopei.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_gyroscopei.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_gyroscopei.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->gyroscope[i]);
       }
       return offset;
@@ -52,23 +56,27 @@ namespace imu_msg
       int offset = 0;
       for( uint32_t i = 0; i < 3; i++){
       union {
-        int16_t real;
-        uint16_t base;
+        float real;
+        uint32_t base;
       } u_accelerometeri;
       u_accelerometeri.base = 0;
-      u_accelerometeri.base |= ((uint16_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_accelerometeri.base |= ((uint16_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_accelerometeri.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_accelerometeri.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_accelerometeri.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_accelerometeri.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
       this->accelerometer[i] = u_accelerometeri.real;
       offset += sizeof(this->accelerometer[i]);
       }
       for( uint32_t i = 0; i < 3; i++){
       union {
-        int16_t real;
-        uint16_t base;
+        float real;
+        uint32_t base;
       } u_gyroscopei;
       u_gyroscopei.base = 0;
-      u_gyroscopei.base |= ((uint16_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_gyroscopei.base |= ((uint16_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_gyroscopei.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_gyroscopei.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_gyroscopei.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_gyroscopei.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
       this->gyroscope[i] = u_gyroscopei.real;
       offset += sizeof(this->gyroscope[i]);
       }
@@ -76,7 +84,7 @@ namespace imu_msg
     }
 
     const char * getType(){ return "imu_msg/imu_light"; };
-    const char * getMD5(){ return "0070330febf44d88d21e1d8326ef9576"; };
+    const char * getMD5(){ return "f0257b4b31db476ff957a2cf40e373be"; };
 
   };
 
